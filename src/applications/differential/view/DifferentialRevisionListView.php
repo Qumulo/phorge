7804aed44a9a8857a0f90df0dcf8851ec0057c0c
Steven Haber <steven@qumulo.com>
2026-01-13 16:27:03 -0800

    Change the commit sizing indicator to be t-shirt based.
    (cherry picked from commit 948b8acf77415ede77a951e66ed621062a55e024)

--- src/applications/differential/view/DifferentialRevisionListView.php
+++ src/applications/differential/view/DifferentialRevisionListView.php
@@ -218,49 +218,19 @@ final class DifferentialRevisionListView extends AphrontView {
       return null;
     }
 
-    $size = array();
-
-    $glyphs = $revision->getRevisionScaleGlyphs();
-    $plus_count = 0;
-    for ($ii = 0; $ii < 7; $ii++) {
-      $c = $glyphs[$ii];
-
-      switch ($c) {
-        case '+':
-          $size[] = id(new PHUIIconView())
-            ->setIcon('fa-plus');
-          $plus_count++;
-          break;
-        case '-':
-          $size[] = id(new PHUIIconView())
-            ->setIcon('fa-minus');
-          break;
-        default:
-          $size[] = id(new PHUIIconView())
-            ->setIcon('fa-square-o invisible');
-          break;
-      }
-    }
-
+    $size = $revision->getRevisionTShirtSize();
+    $display = $revision->getRevisionSizeDisplay();
     $n = $revision->getAddedLineCount() + $revision->getRemovedLineCount();
 
-    $classes = array();
-    $classes[] = 'differential-revision-size';
+    $classes = array('differential-revision-size');
 
-    $tip = array();
-    $tip[] = pht('%s Lines', new PhutilNumber($n));
-
-    if ($plus_count <= 1) {
+    if (strpos($size, 'S') !== false) {
       $classes[] = 'differential-revision-small';
-      $tip[] = pht('Smaller Change');
-    }
-
-    if ($plus_count >= 4) {
+    } else if (strpos($size, 'L') !== false) {
       $classes[] = 'differential-revision-large';
-      $tip[] = pht('Larger Change');
     }
 
-    $tip = phutil_implode_html(" \xC2\xB7 ", $tip);
+    $tip = pht('%s Lines', new PhutilNumber($n));
 
     return javelin_tag(
       'span',
@@ -270,10 +240,10 @@ final class DifferentialRevisionListView extends AphrontView {
         'meta' => array(
           'tip' => $tip,
           'align' => 'E',
-          'size' => 400,
+          'size' => 200,
         ),
       ),
-      $size);
+      $display);
   }
 
 }
